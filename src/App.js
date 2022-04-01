7829a9c61c00add6961f0fd64d1367446f5937bf
Ikenna Okonkwo <ikennaokonkwo2012@gmail.com>
2022-04-01 15:14:01 -0400

    Set up eslint and github actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+/* eslint-disable react/no-array-index-key, react-hooks/exhaustive-deps,
+ react/jsx-filename-extension */
+
 import './App.css';
+import React from 'react';
 
 function App() {
-
   return (
     <div>
       <p>RecipeSearch</p>
