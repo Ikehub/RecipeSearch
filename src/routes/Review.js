d2db106ddf05486e111749d90a0759a6c96510a7
Astrid <adieguez@Andromeda.localdomain>
2022-04-22 17:43:30 -0400

    added review form

---
+++ src/routes/Review.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function App() {
+    function submitReview(){
+        
+    }
+    return (
+        <div>
+            <h1>Review a Recipe</h1>
+            <form onSubmit={submitReview}>
+                <textarea placeholder="Write a review" rows="10" cols="25"></textarea>        
+                <div class="">
+                    <input type="radio" id="1star" name="rating" value="1"/>
+                    <label for="1star">1 star</label>
+                    <input type="radio" id="2star" name="rating" value="2"/>
+                    <label for="2star">2 stars</label>
+                    <input type="radio" id="3star" name="rating" value="3"/>
+                    <label for="3star">3 stars</label>
+                    <input type="radio" id="4star" name="rating" value="4"/>
+                    <label for="4star">4 stars</label>
+                    <input type="radio" id="5star" name="rating" value="5"/>
+                    <label for="5star">5 stars</label>
+                </div>
+            </form>
+            <button type="submit">Submit Review</button>
+        </div>
+
+    );
+}
+
+export default App;
